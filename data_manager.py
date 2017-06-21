c8ee76d602d416d6852d316bf2dd36f7a92a2942
James Brill <jbrill@essex.ac.uk>
2017-06-21 15:53:27 +0100

    initial commit

---
+++ data_manager.py
@@ -0,0 +1,41 @@
+import glob 
+
+
+"""
+Simple class which acts as a wrapper to a python dictionary for the knowledge
+and question banks.
+
+Used by the bot to interface with either the knowledge bank or the question bank
+
+"""
+class Data_manager():
+    """
+    Initialises a new instance of the class. 
+    """
+    def __init__(self, data): 
+        self.__kpath= '/home/james/Documents/data/' # MUST BE CHANGED AS HARDWIRED
+        self.__kpath+=data
+        self.__kpath+='/'
+        self.__extension = ".txt" 
+        self.__knowledge = {} 
+        self.load_data() 
+
+    """
+    loads the data in the supplied folder into a dictionary. 
+    """
+    def load_data(self): 
+        path = self.__kpath + "*" + self.__extension 
+        for fname in glob.glob(path): 
+            f1 = open(fname, 'r') 
+            string =f1.read() 
+            self.__knowledge[fname] = string.decode('utf8', 'ignore').encode('utf8')  
+
+            f1.close() 
+             
+    """
+    gets the content of a file given a specific file name. 
+    """
+    def get_page(self, name):
+        return self.__knowledge[self.__kpath + name + self.__extension] 
+
+
---
+++ simple_bot.py
@@ -0,0 +1,86 @@
+from __future__ import print_function 
+from __future__ import division 
+from chatterbot import ChatBot 
+from chatterbot.trainers import ListTrainer 
+from data_manager import Data_manager 
+import sys
+from time import time 
+
+
+class Simple_bot:
+    """
+    Constructor for the chatbot. Sets up the relevant adapters and loads up data managers to 
+    handle the knowledge banks. 
+    """
+    def __init__(self): 
+        self.__q_bank = Data_manager('questions') 
+        self.__k_bank = Data_manager('knowledge') 
+        self.__chatbot = ChatBot("UKDA Bot", 
+                storage_adapter="chatterbot.storage.JsonFileStorageAdapter", 
+                logic_adapters=[
+                    #                    "chatterbot.logic.MathematicalEvaluation", 
+                    #                    "chatterbot.logic.TimeLogicAdapter", 
+                    "chatterbot.logic.BestMatch"
+                    ], 
+                input_adapter="chatterbot.input.TerminalAdapter",
+                silence_performance_warning=True,  
+                output_adapter="chatterbot.output.TerminalAdapter", 
+                database= "../data/database.db", 
+                )
+        self.__chatbot.set_trainer(ListTrainer)
+        
+        self.train()
+        print("Chatbot created")
+        print("----------------------------------------")
+        print("Ask the bot a question to get a response") 
+        print("----------------------------------------")
+    def load_conversations(self, q,k): 
+        """
+        Loads conversations from the knowledge base using a CSV file which has a 
+        question and an answer. 
+        """
+        path = "/home/james/Documents/data/qa.csv"
+        conversations = [] 
+        csvfile = open(path, 'r') 
+        for line in csvfile:
+            stripped = line.strip("\n") 
+            value = stripped.split(",")
+            conversation = [q.get_page(value[0]), k.get_page(value[1])]  
+            conversations.append(conversation) 
+        csvfile.close() 
+        return conversations 
+
+
+    def train(self):
+        """
+        Trains the chatbot on the questions and answers loaded from it's CSV file. 
+        """
+        conversations = self.load_conversations(self.__q_bank, self.__k_bank)  
+        i = 0 
+        t0 = time() 
+        for conversation in conversations: 
+            i+=1 
+            try:
+                self.__chatbot.train(conversation) 
+            except KeyError: 
+                print("error has occurred on line " , i, "in qa.csv")
+        print("trained on", i, "examples in", round(time()-t0, 3), "s") 
+
+    def run(self):
+        """
+        Runs the chatbot in terminal mode.
+        """
+        while True: 
+            try: 
+                self.__bot_input = self.__chatbot.get_response(None) 
+            except(KeyboardInterrupt, EOFError, SystemExit): 
+#Load and train the chatbot. 
+                break 
+            print("=================================================================")        
+reload(sys) 
+sys.setdefaultencoding('utf8')
+
+print("----------------------------------------")
+bot1 = Simple_bot() 
+
+bot1.run() 
