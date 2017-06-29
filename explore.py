99e59f81c335abf9aacf7a7093de586d961fab38
James Brill <jbrill@essex.ac.uk>
2017-06-29 11:57:23 +0100

    cleaned up some of the files

--- explore.py
+++ explore.py
@@ -1,8 +1,5 @@
 from __future__ import print_function, division
 import pandas as pd  
-import seaborn as sns 
-import matplotlib.pyplot as plt 
-import math 
 
 def get_contents(fname): 
     f1 = open(fname, 'r') 
@@ -11,31 +8,19 @@ def get_contents(fname):
         row = row.strip("\n") 
         row = row.split(",") 
         temp_data.append(row[1])
-    f1.close() 
+    f1.close()  
     return temp_data 
 
 def generate_counts(data): 
-    counts = [] 
-    names = [] 
-
-    for row in data: 
-        if row not in names:
+    counts = []
+    names=[]
+    for row in data:
+        if row not in names: 
             names.append(row) 
             counts.append(1) 
-        else: 
-             for i in range(len(names)): 
-                 if row == names[i]: 
-                     counts[i]+=1
+        else:
+            for i in range(len(names)): 
+                if row == names[i]:
+                    counts[i]+=1 
+
     return pd.DataFrame({"names":names, "count":counts}) 
-
-
-contents = get_contents("data/qa.csv")
-data = generate_counts(contents) 
-print(data)
-#generate the histogram
-s = sns.barplot(x="names", y="count", data=data)
-
-#s.set_xticklabels(rotation=90)
-plt.xticks(rotation=90)
-sns.plt.show()
-
