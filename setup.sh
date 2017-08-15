028cb4c6eb3e76f1ff70b837c16b69a030218ddb
James Brill <jbrill@essex.ac.uk>
2017-08-15 09:43:46 +0100

    updated teh setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 #Set up python virtual env first. 
 
-# =============================================================================
+# ========================================================================
 # Python Packages 
-# =============================================================================
+# ========================================================================
 pip install numpy 
 pip install scipy
 pip install pandas
@@ -19,9 +19,9 @@ pip install h5py
 pip install pickle
 pip install progressbar
 pip install flask 
-# =============================================================================
+# ========================================================================
 # download glove vectors to correct location  
-# =============================================================================
+# ========================================================================
 mkdir vectors
 wget http://nlp.stanford.edu/data/glove.6B.zip 
 unzip glove.6B.zip 
@@ -31,13 +31,19 @@ rm glove.6B.300d.txt
 rm glove.6B.zip
 mkdir vectors/glove
 mv glove.6B.100d.txt vectors/glove/
-# =============================================================================
+# ========================================================================
 # download word2vec vectors to correct location  
-# =============================================================================
+# ========================================================================
 ./misc/download_word2vec.sh
-mkdir vectros/word2vec
+mkdir vectors/word2vec
 gzip -d GoogleNews-vectors-negative300.bin.gz
 rm GoogleNews-vectors-negative300.bin.gz 
 mv GoogleNews-vectors-negative300.bin vectors/word2vec
-
+# ========================================================================
+# set up data directory
+# ========================================================================
+mkdir data
+mkdir data/questions
+mkdir data/knowledge
+touch data/qa.csv
 python test.py
